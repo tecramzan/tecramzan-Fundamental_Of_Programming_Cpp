6cee67283a8b19ab3a4173c7c63ffe91312b1bef
Muhammad Ramzan <veer27420@gmail.com>
2024-11-30 00:10:16 +0500

    Update first.cpp

--- Fundamental_Of_Cpp/first.cpp
+++ Fundamental_Of_Cpp/first.cpp
@@ -1,8 +1,29 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-    cout << "Hellop";
+int main() {
+    string name;
+    int age;
+    double cgp;
+
+     
+    cout << "Enter name: ";
+    cin >> name;   
+
+    cout << "Enter age: ";
+    cin >> age;
+
+    cout << "Enter CGP: ";
+    cin >> cgp;
+
+     
+    cout << "******************" << endl;
+    cout << "*                *" << endl;
+    cout << "*   Name:" << name << "   *" << endl;   
+    cout << "*   Age: " << age << "      *" << endl;    
+    cout << "*   CGP: " << cgp << "     *" << endl;   
+    cout << "*                *" << endl;
+    cout << "******************" << endl;
+
     return 0;
 }
